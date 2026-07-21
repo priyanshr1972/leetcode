43a6f6a1338dea9f0fab654deb43c8ad49eea47d
Priyansh sharma <103410426+priyanshr1972@users.noreply.github.com>
2026-07-21 23:37:36 +0530

    Time: 2 ms (23.83%), Space: 19.3 MB (41.65%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.py
@@ -0,0 +1,17 @@
+def f(i,j,dp):
+        if i==0 and j==0:
+            return 1
+        if i<0 or j<0:
+            return 0
+        if dp[i][j]!=-1:
+            return dp[i][j]
+        
+        up= f(i-1,j,dp)
+        left= f(i,j-1,dp)
+        dp[i][j] = up+left
+        return dp[i][j]
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp = [[-1] * n for _ in range(m)]
+        return f(m-1,n-1,dp)
+    
